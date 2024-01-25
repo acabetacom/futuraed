f8622819c4a9155cb64b478dd07d68dde9bedef1
Daniel Bryte <ezedinirudaniel@gmail.com>
2024-01-25 18:09:46 +0100

    about right section

--- app/components/About.js
+++ app/components/About.js
@@ -1,18 +1,32 @@
-import React from 'react'
 
-export default function 
-() {
+export default function
+  () {
   return (
-    <section className='py-12 bg-white w-full'>
-        <div className='flex justify-evenly'>
-            <div className='border border-black'>
-                <h2 className='capitalize'>about futuraed</h2>
+    <section className='py-28 bg-white w-full'>
+      <div className='grid grid-cols-1 md:grid-cols-2 px-2 gap-8 container mx-auto'>
+
+        <div className='px-6'>
+          <h2 className='font-bold text-4xl pb-9'>About FuturaEd</h2>
+          <div className="flex flex-col gap-8">
+
+
+            <p className="text-neutral-800 text-xl font-normal font-['Inter'] capitalize">
+              FuturaEd is an annual event that brings together important voices impacting  Education direct and indirectly, to discuss, bring solution to challenges in edtech, and change to impact the future of education.
+            </p>
+            <p className="text-neutral-800  text-xl font-normal font-['Inter'] capitalize">All students and techies are advised to come, including job seekers, startup founders, companies and more. there will be hirings, free laptops, data, courses, and investors. There will be a talent pool opening for attendees 3 weeks before the event, and this will close 1 week before the event.
+            </p>
+
+            <div className="mt-4">
+              <a href='' className='font-bold bg-[#2BA10E] hover:bg-[#2ba10ece]  hover:scale-105 duration-700 py-3.5 px-3 border text-white border-black rounded-lg cursor-pointer'><button>Get Your Tickets</button></a>
             </div>
 
-            <div className='border border-black'>
-
-            </div>
+          </div>
         </div>
+
+        <div className='border border-black px-6'>
+
+        </div>
+      </div>
     </section>
   )
 }
